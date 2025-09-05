d6156792a3382a7149449f3230bed7f4137acdad
yeger00 <yeger00@gmail.com>
2025-09-05 13:58:12 +0300

    Add some todos

--- src/aython/aython_agent.py
+++ src/aython/aython_agent.py
@@ -4,6 +4,22 @@ import litellm
 from pydantic import BaseModel
 
 
+class StaticAnalysis:
+    def check(self, code_snippet):
+        pass
+
+    def fix(self, code_snippet):
+        pass
+
+# TODO: understand if this is possible?
+class DynamicAnalysis:
+    def check(self, code_snippet):
+        pass
+
+    def fix(self, code_snippet):
+        pass
+
+
 class CodeResult(BaseModel):
     """A model to hold the generated code snippet."""
     code_snippet: str
@@ -42,7 +58,8 @@ class AythonAgent():
             tool_call = response.choices[0].message.tool_calls[0]
             arguments = json.loads(tool_call.function.arguments)
             generated_code = CodeResult(**arguments).code_snippet
-
+            # TODO: use static and dynamic analysis in order to verify the code
+            # TODO: if the validation fail, use the llm again up to 3 tries.
             return generated_code
         except Exception as e:
             print(f"Error during code generation: {e}")
